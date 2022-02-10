7859366ca49394fe138ebada84d861c329be1ebd
mrzhenshen <mrzhenshen@gmail.com>
2022-02-10 18:16:06 +0200

    done task2,3,4, create StringController

---
+++ src/main/java/task_2/StringController.java
@@ -0,0 +1,25 @@
+package task_2;
+
+import java.util.ArrayList;
+
+public class StringController {
+
+    public static int wordsAmount(String text) {
+        return text.split(" ").length;
+    }
+
+    public static ArrayList<Integer> charsAmount(String text) {
+        ArrayList<Integer> charsInWords = new ArrayList<>();
+        String[] words = text.split(" ");
+        for (String word : words) {
+            charsInWords.add(word.codePointCount(0, word.length()));
+        }
+        return charsInWords;
+    }
+
+    public static int charsAmountByIndex(String text, int index) {
+        return text.substring(index).split(" ")[0].length();
+    }
+
+
+}
