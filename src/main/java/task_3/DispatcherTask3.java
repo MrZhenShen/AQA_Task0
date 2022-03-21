1bc75748352f9f67cbc70f6718d51064d6ed849d
mrzhenshen <mrzhenshen@gmail.com>
2022-03-21 17:37:35 +0200

    init task_3. create ArrayController for Array task

--- src/main/java/task_3/DispatcherTask3.java
+++ src/main/java/task_3/DispatcherTask3.java
@@ -2,6 +2,13 @@ package task_3;
 
 public class DispatcherTask3 {
     public static void main(String[] args) {
+        double[] array1 = {5, 9, 3, 2, 0, -4, -8.3, 3, -4};
+        int[] array2 = {1, 6, 3, 3, 4, 5, 5, 6, 0};
+        String[] array3 = {"12", "23", "34", "12", "56", "67"};
 
+        System.out.println(ArrayController.findMax(array1));
+        ArrayController.showWithoutDuplicates(array2);
+        View.showArray(ArrayController.replaceToString(array3));
+        ArrayController.showReversed(array2);
     }
 }
---
+++ src/main/java/task_3/View.java
@@ -0,0 +1,9 @@
+package task_3;
+
+public class View {
+    public static void showArray(Object[] o) {
+        for (Object v : o) {
+            System.out.print(v + " ");
+        }
+    }
+}
